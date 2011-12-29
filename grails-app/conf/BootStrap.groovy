55be1a28cf4a8a86de710fdf58bbe971cf74ea1b
Riccardo Focardi <riccardo.focardi@gmail.com>
2011-12-29 17:00:08 +0100

    first commit

---
+++ grails-app/conf/BootStrap.groovy
@@ -0,0 +1,14 @@
+import org.example.Book
+
+class BootStrap {
+
+    def init = { servletContext ->
+        // Check whether the test data already exists.
+        if (!Book.count()) {
+            new Book(author: "Stephen King", title: "The Shining").save(failOnError: true)
+            new Book(author: "James Patterson", title: "Along Came a Spider").save(failOnError: true)
+        }
+    }
+    def destroy = {
+    }
+}
